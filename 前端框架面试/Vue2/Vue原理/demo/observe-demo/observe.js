91f05935fe1aa08590f0c69d6505927adc79052b
xing-6464 <xing_6464@foxmail.com>
2022-11-22 02:13:41 +0800

    vue如何监听数组变化

--- 前端框架面试/Vue2/Vue原理/demo/observe-demo/observe.js
+++ 前端框架面试/Vue2/Vue原理/demo/observe-demo/observe.js
@@ -5,13 +5,13 @@ function updateView() {
 
 // 重新定义数组原型
 const oldArrayProperty = Array.prototype
-// 创建新对象，原型指向 oldArrayProperty ，再扩展新的方法不会影响原型
+// 创建新对象，原型指向 oldArrayProperty, 再扩展新的方法不会影响原型
 const arrProto = Object.create(oldArrayProperty);
-['push', 'pop', 'shift', 'unshift', 'splice'].forEach(methodName => {
+['push', 'pop', 'shuift', 'unshufit', 'splice'].forEach(methodName => {
     arrProto[methodName] = function () {
         updateView() // 触发视图更新
+        // Array.property.push.call(this, ...arguments)
         oldArrayProperty[methodName].call(this, ...arguments)
-        // Array.prototype.push.call(this, ...arguments)
     }
 })
 
@@ -53,7 +53,10 @@ function observer(target) {
     //     updateView()
     //     ...
     // }
-
+    
+    if (Array.isArray(target)) {
+        target.__proto__ = arrProto
+    }
 
     // 重新定义各个属性（for in 也可以遍历数组）
     for (let key in target) {
@@ -68,17 +71,17 @@ const data = {
     info: {
         address: '北京' // 需要深度监听
     },
-    // nums: [10, 20, 30]
+    nums: [10, 20, 30]
 }
 
 // 监听数据
 observer(data)
 
 // 测试
-data.name = 'lisi'
-data.age = 21
+// data.name = 'lisi'
+// data.age = 21
 // // console.log('age', data.age)
-data.x = '100' // 新增属性，监听不到 —— 所以有 Vue.set
-delete data.name // 删除属性，监听不到 —— 所有已 Vue.delete
-data.info.address = '上海' // 深度监听
+// data.x = '100' // 新增属性，监听不到 —— 所以有 Vue.set
+// delete data.name // 删除属性，监听不到 —— 所有已 Vue.delete
+// data.info.address = '上海' // 深度监听
 // data.nums.push(4) // 监听数组
